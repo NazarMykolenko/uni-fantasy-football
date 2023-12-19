6a648674e21f9e53f637ddf9a2ec936befbe287d
masalovd <dimkamasalov225@gmail.com>
2023-12-19 11:39:55 +0200

    Fix database structure

--- server/db/structure.sql
+++ server/db/structure.sql
@@ -1,18 +1,18 @@
-CREATE TABLE "Users" (
+CREATE TABLE "users" (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE "Teams" (
+CREATE TABLE "teams" (
     team_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES "Users"(user_id),
+    user_id INT REFERENCES "users"(user_id),
     team_name VARCHAR(20) NOT NULL,
     budget DECIMAL(10, 2) DEFAULT 100.00,
     total_coefficient DECIMAL(5, 2) DEFAULT 0.00
 );
 
-CREATE TABLE "Players" (
+CREATE TABLE "players" (
     player_id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     team VARCHAR(50) NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE "Players" (
     rating DECIMAL(4, 2) DEFAULT 0.00
 );
 
-CREATE TABLE "TeamPlayers" (
-    team_id INT REFERENCES "Teams"(team_id),
-    player_id INT REFERENCES "Players"(player_id),
+CREATE TABLE "team_players" (
+    team_id INT REFERENCES "teams"(team_id),
+    player_id INT REFERENCES "players"(player_id),
     PRIMARY KEY (team_id, player_id)
 );
 
