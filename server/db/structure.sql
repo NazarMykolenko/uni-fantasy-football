44ad45dc93ea27fa17232fb6a1a76fbcf0c63e95
masalovd <dimkamasalov225@gmail.com>
2023-12-19 20:41:19 +0200

    Modify structure, add utils for request to db, add server functionality

--- server/db/structure.sql
+++ server/db/structure.sql
@@ -4,26 +4,66 @@ CREATE TABLE "users" (
     password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE "teams" (
-    team_id SERIAL PRIMARY KEY,
+CREATE TABLE "user_teams" (
+    user_team_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES "users"(user_id),
-    team_name VARCHAR(20) NOT NULL,
+    user_team_name VARCHAR(20) NOT NULL,
     budget DECIMAL(10, 2) DEFAULT 100.00,
     total_coefficient DECIMAL(5, 2) DEFAULT 0.00
 );
 
+CREATE TABLE "official_teams" (
+    official_team_id INT PRIMARY KEY,
+    official_team_name VARCHAR(20) NOT NULL,
+    official_team_name_short VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE "player_positions" (
+    position_id INT PRIMARY KEY,
+    position_name VARCHAR(15) NOT NULL,
+    position_name_short VARCHAR(3) NOT NULL
+);
+
+
 CREATE TABLE "players" (
     player_id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    team VARCHAR(50) NOT NULL,
-    position VARCHAR(20) NOT NULL,
+    official_team_id INT REFERENCES "official_teams"(official_team_id),
+    position_id INT REFERENCES "player_positions"(position_id),
     price DECIMAL(8, 2) NOT NULL,
-    rating DECIMAL(4, 2) DEFAULT 0.00
+    rating DECIMAL(10, 2) DEFAULT 0.00,
+    chance_of_playing_next_round INT,
+    chance_of_playing_this_round INT,
+    minutes INT,
+    goals_scored INT,
+    assists INT,
+    goals_conceded INT,
+    penalties_saved INT,
+    penalties_missed INT,
+    yellow_cards INT,
+    red_cards INT,
+    saves INT,
+    bonus INT,
+    influence VARCHAR(10),
+    creativity VARCHAR(10),
+    threat VARCHAR(10),
+    ict_index VARCHAR(10),
+    expected_goals VARCHAR(10),
+    expected_assists VARCHAR(10),
+    expected_goal_involvements VARCHAR(10),
+    expected_goals_conceded VARCHAR(10),
+    expected_goals_per_90 DECIMAL(5, 2),
+    saves_per_90 DECIMAL(5, 2),
+    expected_assists_per_90 DECIMAL(5, 2),
+    expected_goal_involvements_per_90 DECIMAL(5, 2),
+    expected_goals_conceded_per_90 DECIMAL(5, 2),
+    goals_conceded_per_90 DECIMAL(5, 2),
+    starts_per_90 DECIMAL(5, 2),
+    clean_sheets_per_90 DECIMAL(5, 2)
 );
 
 CREATE TABLE "team_players" (
-    team_id INT REFERENCES "teams"(team_id),
+    user_team_id INT REFERENCES "user_teams"(user_team_id),
     player_id INT REFERENCES "players"(player_id),
-    PRIMARY KEY (team_id, player_id)
+    PRIMARY KEY (user_team_id, player_id)
 );
-
---
+++ server/db/utils.js
@@ -0,0 +1,142 @@
+async function dbGetAllPlayers() {
+  const query = `SELECT * FROM "players"`;
+  const result = await client.query(query);
+  return result.rows;
+}
+
+async function dbGetOfficialTeams() {
+  const query = `SELECT * FROM "official_teams"`;
+  const result = await client.query(query);
+  return result.rows;
+}
+
+async function dbGetPlayerPositions() {
+  const query = `SELECT * FROM "player_positions"`;
+  const result = await client.query(query);
+  return result.rows;
+}
+
+async function insertOfficialTeam(client, official_team) {
+  const query = `
+  INSERT INTO "official_teams" (official_team_id, official_team_name, official_team_name_short)
+  VALUES ($1, $2, $3)
+`;
+
+  const values = [
+    official_team.id,
+    official_team.name,
+    official_team.short_name,
+  ];
+  const result = await client.query(query, values);
+  return result.rows;
+}
+
+async function insertPlayerPosition(client, player_position) {
+  const query = `
+ INSERT INTO "player_positions" (position_id, position_name, position_name_short)
+ VALUES ($1, $2, $3)
+`;
+
+  const values = [
+    player_position.id,
+    player_position.singular_name,
+    player_position.singular_name_short,
+  ];
+  const result = await client.query(query, values);
+  return result.rows;
+}
+
+async function upsertPlayerInfo(client, player) {
+  const query = `
+   INSERT INTO players (
+     player_id, name, official_team_id, position_id, price, rating,
+     chance_of_playing_next_round, chance_of_playing_this_round, minutes,
+     goals_scored, assists, goals_conceded, penalties_saved, penalties_missed,
+     yellow_cards, red_cards, saves, bonus, influence, creativity, threat,
+     ict_index, expected_goals, expected_assists, expected_goal_involvements,
+     expected_goals_conceded, expected_goals_per_90, saves_per_90,
+     expected_assists_per_90, expected_goal_involvements_per_90,
+     expected_goals_conceded_per_90, goals_conceded_per_90, starts_per_90,
+     clean_sheets_per_90
+   ) VALUES (
+     $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16,
+     $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30,
+     $31, $32, $33, $34
+   )
+   ON CONFLICT (player_id) DO UPDATE
+   SET (
+     player_id, name, official_team_id, position_id, price, rating, chance_of_playing_next_round,
+     chance_of_playing_this_round, minutes, goals_scored, assists, goals_conceded,
+     penalties_saved, penalties_missed, yellow_cards, red_cards, saves, bonus,
+     influence, creativity, threat, ict_index, expected_goals, expected_assists,
+     expected_goal_involvements, expected_goals_conceded, expected_goals_per_90,
+     saves_per_90, expected_assists_per_90, expected_goal_involvements_per_90,
+     expected_goals_conceded_per_90, goals_conceded_per_90, starts_per_90,
+     clean_sheets_per_90
+   ) = (
+     EXCLUDED.player_id, EXCLUDED.name, EXCLUDED.official_team_id, EXCLUDED.position_id,
+     EXCLUDED.price, EXCLUDED.rating, EXCLUDED.chance_of_playing_next_round,
+     EXCLUDED.chance_of_playing_this_round, EXCLUDED.minutes, EXCLUDED.goals_scored,
+     EXCLUDED.assists, EXCLUDED.goals_conceded, EXCLUDED.penalties_saved,
+     EXCLUDED.penalties_missed, EXCLUDED.yellow_cards, EXCLUDED.red_cards,
+     EXCLUDED.saves, EXCLUDED.bonus, EXCLUDED.influence, EXCLUDED.creativity,
+     EXCLUDED.threat, EXCLUDED.ict_index, EXCLUDED.expected_goals,
+     EXCLUDED.expected_assists, EXCLUDED.expected_goal_involvements,
+     EXCLUDED.expected_goals_conceded, EXCLUDED.expected_goals_per_90,
+     EXCLUDED.saves_per_90, EXCLUDED.expected_assists_per_90,
+     EXCLUDED.expected_goal_involvements_per_90,
+     EXCLUDED.expected_goals_conceded_per_90, EXCLUDED.goals_conceded_per_90,
+     EXCLUDED.starts_per_90, EXCLUDED.clean_sheets_per_90
+   )
+   RETURNING player_id;
+ `;
+
+  const values = [
+    player.id,
+    player.first_name + " " + player.second_name,
+    player.team,
+    player.element_type,
+    player.now_cost,
+    player.total_points,
+    player.chance_of_playing_next_round,
+    player.chance_of_playing_this_round,
+    player.minutes,
+    player.goals_scored,
+    player.assists,
+    player.goals_conceded,
+    player.penalties_saved,
+    player.penalties_missed,
+    player.yellow_cards,
+    player.red_cards,
+    player.saves,
+    player.bonus,
+    player.influence,
+    player.creativity,
+    player.threat,
+    player.ict_index,
+    player.expected_goals,
+    player.expected_assists,
+    player.expected_goal_involvements,
+    player.expected_goals_conceded,
+    player.expected_goals_per_90,
+    player.saves_per_90,
+    player.expected_assists_per_90,
+    player.expected_goal_involvements_per_90,
+    player.expected_goals_conceded_per_90,
+    player.goals_conceded_per_90,
+    player.starts_per_90,
+    player.clean_sheets_per_90,
+  ];
+  const result = await client.query(query, values);
+  return result.rows;
+}
+
+module.exports = {
+  dbGetPlayers,
+  dbGetOfficialTeams,
+  dbGetPlayerPositions,
+  insertOfficialTeam,
+  insertPlayerPosition,
+  insertPlayerPosition,
+  upsertPlayerInfo,
+};
