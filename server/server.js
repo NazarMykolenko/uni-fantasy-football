e4352ae55ae7855ea53b6b1c83e69b339ee122a0
masalovd <dimkamasalov225@gmail.com>
2023-12-19 20:45:25 +0200

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+const {
+  upsertPlayerInfo,
+  insertOfficialTeam,
+  insertPlayerPosition,
+  dbGetOfficialTeams,
+  dbGetPlayerPositions,
+} = require("./db/utils");
+
 const fastify = require("fastify")({
   logger: {
     transport: {
@@ -18,4 +26,56 @@ fastify.get("/", async (_, reply) => reply.send("Fantasy football ⚽️"));
 fastify.listen({ port: 8000 }, (err) => {
   if (err) throw err;
   console.log(`server listening on ${fastify.server.address().port}`);
+});
+
+const REFRESH_MS = 10000;
+const URL_API_FANTASY_FOOTBALL =
+  "https://fantasy.premierleague.com/api/bootstrap-static/";
+
+const interval = setInterval(job, REFRESH_MS);
+
+async function job() {
+  const client = await fastify.pg.connect();
+  const response = await fetch(URL_API_FANTASY_FOOTBALL);
+  const data = await response.json();
+
+  if (!client.dbGetOfficialTeams()) {
+    for (team of data.team) {
+      const result = await insertOfficialTeam(client, team);
+      console.log(JSON.stringify(result));
+    }
+  }
+
+  if (!client.dbGetAllPlayers()) {
+    for (team of data.element_types) {
+      const result = await insertPlayerPosition(client, team);
+      console.log(JSON.stringify(result));
+    }
+  }
+
+  for (const player of data.elements) {
+    const result = await upsertPlayerInfo(client, player);
+    console.log(JSON.stringify(result));
+  }
+}
+
+fastify.get("/players", async (_, reply) => {
+  const client = await fastify.pg.connect();
+  const players = await dbGetAllPlayers(client);
+  reply.send(players);
+  client.release();
+});
+
+fastify.get("/official_teams", async (_, reply) => {
+  const client = await fastify.pg.connect();
+  const official_teams = await dbGetOfficialTeams(client);
+  reply.send(official_teams);
+  client.release();
+});
+
+fastify.get("/player_positions", async (_, reply) => {
+  const client = await fastify.pg.connect();
+  const player_positions = await dbGetPlayerPositions(client);
+  reply.send(player_positions);
+  client.release();
 });
