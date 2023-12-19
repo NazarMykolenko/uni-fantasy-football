77a7235e4497d148cab3c5fba4c09ba005172a6a
masalovd <dimkamasalov225@gmail.com>
2023-12-19 11:27:10 +0200

    Add database structure

--- server/db/structure.sql
+++ server/db/structure.sql
@@ -1,7 +1,29 @@
-CREATE TABLE "object"(
-    object_id serial PRIMARY KEY,
-    name varchar(255),
-    location varchar(255),
-    UNIQUE (name)
+CREATE TABLE "Users" (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE "Teams" (
+    team_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "Users"(user_id),
+    team_name VARCHAR(20) NOT NULL,
+    budget DECIMAL(10, 2) DEFAULT 100.00,
+    total_coefficient DECIMAL(5, 2) DEFAULT 0.00
+);
+
+CREATE TABLE "Players" (
+    player_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    team VARCHAR(50) NOT NULL,
+    position VARCHAR(20) NOT NULL,
+    price DECIMAL(8, 2) NOT NULL,
+    rating DECIMAL(4, 2) DEFAULT 0.00
+);
+
+CREATE TABLE "TeamPlayers" (
+    team_id INT REFERENCES "Teams"(team_id),
+    player_id INT REFERENCES "Players"(player_id),
+    PRIMARY KEY (team_id, player_id)
 );
 
